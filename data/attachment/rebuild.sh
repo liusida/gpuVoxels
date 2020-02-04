5a11f6c56365b3ed045dbfc0ab8b1ee342d2003e
sida <sliu1@uvm.edu>
2020-02-04 11:08:37 -0500

    demo experiment

--- data/attachment/rebuild.sh
+++ data/attachment/rebuild.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 cwd=$(pwd)
-cd ../../
+
+# For Voxelyze3 (Server side)
+cd ../../Voxelyze3/
 if [ "$1" = "-f" ]; then
     echo "Rebuild all."
     rm build/ -rf
@@ -16,4 +18,20 @@ cmake --build . -j 10
 cd $cwd
 cp ../../build/Voxelyze3/Voxelyze3 .
 cp ../../build/Voxelyze3/vx3_node_worker .
+
+# For VoxCAD (Client side)
+cd ../../VoxCAD/
+if [ "$1" = "-f" ]; then
+    echo "Rebuild all."
+    rm build/ -rf
+    mkdir build
+else
+    echo "Just make."
+fi
+cd build
+if [ "$1" = "-f" ]; then
+    cmake -DCMAKE_BUILD_TYPE=Release ..
+fi
+cmake --build . -j 10
+cd $cwd
 cp ../../build/VoxCAD/VoxCAD .
