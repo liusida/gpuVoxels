5d4c57ebbb7d6ff8a29cbfdb179bc93f899c68b7
sida <sliu1@uvm.edu>
2020-03-01 12:39:58 -0500

    3x3x3 level 4

--- Experiments/xenobot/6.fractal/main.py
+++ Experiments/xenobot/6.fractal/main.py
@@ -17,11 +17,12 @@ def toBinary(number, bit):
 
 # read in the 6x6x5
 def read_robot():
-    root = etree.parse("assets/6x6x5.data")
+    # root = etree.parse("assets/6x6x5.data")
+    root = etree.parse("assets/3x3x3.data")
     morphology = []
     try:
         data = root.findall(".//Data")[0]
-        phaseoffset = root.findall(".//PhaseOffset")[0]
+        # phaseoffset = root.findall(".//PhaseOffset")[0]
         X_Voxels = int(root.findall(".//X_Voxels")[0].text)
         Y_Voxels = int(root.findall(".//Y_Voxels")[0].text)
         Z_Voxels = int(root.findall(".//Z_Voxels")[0].text)
@@ -33,26 +34,40 @@ def read_robot():
     for l in data.iter("Layer"):
         layer = []
         for c in l.text:
-            layer.append( 3 if c=='9' else 0 )
+            layer.append( 1 if c=='1' else 0 )
         morphology.append(layer)
 
     morphology = np.array(morphology)
     morphology = np.array(morphology).reshape(Z_Voxels, Y_Voxels, X_Voxels)
 
-    control = []
-    for l in phaseoffset.iter("Layer"):
-        layer = []
-        values = l.text.split(', ')[:-1]
-        layer = [float(i) for i in values]
-        control.append(layer)
+    # control = []
+    # for l in phaseoffset.iter("Layer"):
+    #     layer = []
+    #     values = l.text.split(', ')[:-1]
+    #     layer = [float(i) for i in values]
+    #     control.append(layer)
 
-    control = np.array(control)
-    control = np.array(control).reshape(Z_Voxels, Y_Voxels, X_Voxels)
+    # control = np.array(control)
+    # control = np.array(control).reshape(Z_Voxels, Y_Voxels, X_Voxels)
+    control = np.random.random([Z_Voxels, Y_Voxels, X_Voxels])
     return morphology, control
 
 body, control = read_robot()
-body1 = body.copy()
-body1[body==3] = 4
+body2 = body.copy()
+body3 = body.copy()
+body4 = body.copy()
+body5 = body.copy()
+body6 = body.copy()
+body7 = body.copy()
+body8 = body.copy()
+
+body2[body==1] = 2
+body3[body==1] = 3
+body4[body==1] = 4
+body5[body==1] = 5
+body6[body==1] = 6
+body7[body==1] = 7
+body8[body==1] = 8
 z,y,x = body.shape
 
 
@@ -64,21 +79,37 @@ def fractal(original_body, body, body1, control):
     z,y,x = original_body.shape
     Z,Y,X = body.shape
     body_2nd = np.zeros([z*Z,y*Y,x*X], dtype=int)
-    body1_2nd = np.zeros([z*Z,y*Y,x*X], dtype=int)
     control_2nd = np.zeros([z*Z,y*Y,x*X], dtype=int)
     for i in range(z):
         for j in range(y):
             for k in range(x):
                 if original_body[i,j,k]!=0:
                     body_2nd[i*Z:(i+1)*Z, j*Y:(j+1)*Y, k*X:(k+1)*X] = body if c%2==0 else body1
-                    body1_2nd[i*Z:(i+1)*Z, j*Y:(j+1)*Y, k*X:(k+1)*X] = body if c%2==1 else body1
                     control_2nd[i*Z:(i+1)*Z, j*Y:(j+1)*Y, k*X:(k+1)*X] = control
                 c+=1
-            c+=1
-        c+=1
-    return body_2nd, body1_2nd, control_2nd
-body_2nd, body1_2nd, control_2nd = fractal(body, body, body1, control)
-world, _, world_control = fractal(body, body_2nd, body1_2nd, control_2nd)
+            # c+=1
+        # c+=1
+    return body_2nd, control_2nd
+level = 4
+if (level==4):
+    bodya_2nd, control_2nd = fractal(body, body, body2, control)
+    bodyb_2nd, _ = fractal(body, body3, body4, control)
+    bodyc_2nd, _ = fractal(body, body5, body6, control)
+    bodyd_2nd, _ = fractal(body, body7, body8, control)
+    
+    body_3rd, control_3rd = fractal(body, bodya_2nd, bodyb_2nd, control_2nd)
+    body1_3rd, _ = fractal(body, bodyc_2nd, bodyd_2nd, control_2nd)
+    world, world_control = fractal(body, body_3rd, body1_3rd, control_3rd)
+if (level==3):
+    body_2nd, control_2nd = fractal(body, body, body4, control)
+    body1_2nd, _ = fractal(body, body5, body6, control)
+    world, world_control = fractal(body, body_2nd, body1_2nd, control_2nd)
+elif(level==2):
+    world, world_control = fractal(body, body, body4, control)
+elif(level==1):
+    world = body
+    world_control = control
+
 Z,Y,X = world.shape
 
 safe_mkdir(f"data")
